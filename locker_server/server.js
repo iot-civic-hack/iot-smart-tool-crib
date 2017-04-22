113b243a4eb68f01529abf9e433c5eca0567113d
Drew Westrick <drewwestrick@glassboard.com>
2017-04-22 13:54:46 +0000

    Minor update

--- locker_server/server.js
+++ locker_server/server.js
@@ -3,8 +3,11 @@ var app = express();
 var gpio = require('rpi-gpio');
 var output = 0;
 
-gpio.setup(7, gpio.DIR_OUT, write);
+gpio.setup(38, gpio.DIR_OUT, write);
 gpio.setup(16, gpio.DIR_IN, gpio.EDGE_BOTH);
+gpio.setup(31, gpio.DIR_OUT);
+gpio.setup(33, gpio.DIR_OUT);
+gpio.setup(35, gpio.DIR_OUT);
 
 app.get('/', function (req, res) {
   res.send('OK');
@@ -31,11 +34,14 @@ app.get('/unlock', function (req, res) {
 
 gpio.on('change', function(channel, value) {
     console.log('Channel ' + channel + ' value is now ' + value);
-    gpio.write(7, value);
+    //gpio.write(38, value);
+    gpio.write(31, true);
+    gpio.write(33, value);
+    gpio.write(35, value);
 });
 
 function write() {
-    gpio.write(7, true, function(err) {
+    gpio.write(38, true, function(err) {
         if (err) throw err;
         console.log('Written to pin');
     });
