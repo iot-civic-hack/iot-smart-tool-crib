8e07db044116c3d4d5bd33a1803413b24f001871
Drew Westrick <drewwestrick@glassboard.com>
2017-04-22 13:40:33 +0000

    Test Input/Output working

--- locker_server/server.js
+++ locker_server/server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+var gpio = require('rpi-gpio');
+var output = 0;
+
+gpio.setup(7, gpio.DIR_OUT, write);
+gpio.setup(16, gpio.DIR_IN, gpio.EDGE_BOTH);
 
 app.get('/', function (req, res) {
   res.send('OK');
@@ -8,7 +13,8 @@ app.get('/', function (req, res) {
 app.get('/unlock', function (req, res) {
 
   var lockerID = req.param('id');
-
+  output = !output;
+  gpio.write(7, output);
 
 
   console.warn("locker id: ", lockerID);
@@ -20,9 +26,21 @@ app.get('/unlock', function (req, res) {
 
 
 
-  res.send('OK');
+  res.send(output);
 });
 
+gpio.on('change', function(channel, value) {
+    console.log('Channel ' + channel + ' value is now ' + value);
+    gpio.write(7, value);
+});
+
+function write() {
+    gpio.write(7, true, function(err) {
+        if (err) throw err;
+        console.log('Written to pin');
+    });
+}
+//gpio.destroy();
 app.listen(3000, function () {
   console.log('Example app listening on port 3000');
 });
